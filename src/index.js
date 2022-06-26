936b2cec24277a9a6608b107cb349787f790c45d
bulgakovatodays <lesyataranina@gmail.com>
2022-06-26 20:25:19 +0300

    init: create index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let a = Math.abs(n);
+  let massiv = String(a).split('');
+  //console.log(massiv);
+  let vissam = massiv.reverse();
+  //console.log(vissam);
+  let newN = vissam.toString('');
+  //console.log(newN);
+  let string = newN.replace(/,/g, '')
+  //console.log(string);
+  let number = Number(string);
+  return number;
 }
